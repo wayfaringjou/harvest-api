12cf05871876be9eb55162767b74c8b8d3615585
Joel Del Cueto <wayfaringjou@gmail.com>
2021-03-04 18:40:57 -0500

    Add simple endpoints for plants

--- src/app.js
+++ src/app.js
@@ -71,4 +71,75 @@ app.route('/api/garden/areas/:areaId')
     res.status(200).json(response);
   });
 
+const serializePlant = (plant) => ({
+  id: plant.id,
+  name: xss(plant.name),
+});
+
+app.route('/api/garden/plants')
+  .get(async (req, res) => {
+    const knex = req.app.get('db');
+    const plants = await knex.select('*').from('plants');
+    const parsedPlants = plants.map(serializePlant);
+    console.log(parsedPlants);
+    res.status(200).json(parsedPlants);
+  })
+  .post(async (req, res) => {
+    const knex = req.app.get('db');
+    const { name } = req.body;
+    const newPlant = {
+      name,
+    };
+    const [response] = await knex.insert(newPlant)
+      .into('plants')
+      .returning('*');
+    res
+      .status(200)
+      .location(`${req.originalUrl}/${response.id}`)
+      .json(response);
+  });
+
+app.route('/api/garden/plants/:plantId')
+  .delete(async (req, res) => {
+    const knex = req.app.get('db');
+    console.log(req.params.plantId);
+    const [response] = await knex('plants')
+      .where('id', req.params.plantId)
+      .delete()
+      .returning('*');
+    console.log(response);
+    res.status(200).json(response);
+  })
+  .patch(async (req, res) => {
+    const knex = req.app.get('db');
+    const [response] = await knex('plants')
+      .where('id', req.params.plantId)
+      .update(req.body)
+      .returning('*');
+    console.log(response);
+    res.status(200).json(response);
+  });
+
+const fetch = require('node-fetch');
+
+app.route('/auth/trefle')
+  .get(async (req, res) => {
+    // The parameters for our POST request
+    const params = {
+      origin: 'http://localhost:3000',
+      // ip: 'THE-WEBSITE-USER-IP',
+      token: 'rpSbG-As7QHC5sqn1E3-x-p7FjDXW82T250KmCGl3EE',
+    };
+    const response = await fetch(
+      'https://trefle.io/api/auth/claim', {
+        method: 'post',
+        body: JSON.stringify(params),
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+    const json = await response.json();
+    console.log(json);
+    res.status(200).json(json);
+  });
+
 module.exports = app;
